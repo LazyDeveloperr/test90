374c9ea9ecbd3434ce66d1e4f0373f865bc9e8d3
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-12-22 13:36:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-cd /app
-echo "----- Now deployed web booting your repo ------ " 
-gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /LazyPrincess
+fi
+cd /LazyPrincess
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
