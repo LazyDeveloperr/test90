47c604ca3fc3252921a2ed030264677d899393c0
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-12-22 12:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://LazyDeveloperr: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@github.com/LazyDeveloperr/LazyPrincess ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://LazyDeveloperr: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@github.com/LazyDeveloperr/test90 ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
